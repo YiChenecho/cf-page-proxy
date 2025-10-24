49d35d22b647a3f54cd2a62c2fbb8273d7a90a5b
FengWei Sun <89239396+YiChenecho@users.noreply.github.com>
2025-10-24 22:37:49 +0800

    Update hostname in fetch request

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="bai-piao-wang-zhe-iplc2.98848.xyz";
+        url.hostname="ibyctdwlpmru.ap-northeast-1.clawcloudrun.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
@@ -10,3 +10,4 @@ export default {
       return env.ASSETS.fetch(request);
     }
   };
+
